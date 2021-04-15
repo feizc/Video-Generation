1ab1ad896adc175ec81f8f5f7f037d4b84c46128
alenfei(费政聪) <alenfei@tencent.com>
2021-04-15 21:49:30 +0800

    video generation

--- video_preprocess.py
+++ video_preprocess.py
@@ -17,9 +17,29 @@ def video_read(video_path, save_path, n):
             img_name = str(int(i / sample_freq)) + '.png' 
             cur_save_path = os.path.join(save_path, img_name)
             cv2.imwrite(cur_save_path, frame)
+
+
+# 根据图片位置list(有顺序)生成视频
+def video_generate(img_path_list, save_path, fps):
+    # 确定图片的大小 
+    img_t = cv2.imread(img_path_list[0]) 
+    # print(img_t.shape) 
+    height, width, channel = img_t.shape 
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v') 
+    videowriter = cv2.VideoWriter(save_path, fourcc, fps, (width, height)) 
+
+    for img_path in img_path_list: 
+        img = cv2.imread(img_path) 
+        videowriter.write(img) 
     
-    
+    videowriter.release()
+
 
 
 if __name__ == "__main__":
-    video_read('data/ORONbxQm7NM_000001_000011.mp4', 'data', 24)
+    # video_read('data/1.mp4', 'data', 24) 
+    img_path = 'data' 
+    img_name_list = os.listdir(img_path) 
+    img_name_list = [os.path.join(img_path, name) for name in img_name_list if 'png' in name]
+    # print(img_name_list)
+    video_generate(img_name_list, 'data/2.mp4', 5)
